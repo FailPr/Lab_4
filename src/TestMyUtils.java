2ac0191e50434768c0e76003a88135c5275ece11
FailPr <102379340+FailPr@users.noreply.github.com>
2022-03-25 19:13:30 +0200

    https://github.com/FailPr/Lab_4.git

--- src/TestMyUtils.java
+++ src/TestMyUtils.java
@@ -4,7 +4,7 @@ public class TestMyUtils
     {
         //region Δημιουργία πίνακα και εισαγωγή φοιτητών
         Foititis[] MyStudentArray = new Foititis[10];
-        MyStudentArray[0]=new Foititis("Jadine","Hinton", (short) 2,2020);
+        MyStudentArray[0]=new Foititis("Jadinee","Hinton", (short) 2,2020);
         MyStudentArray[1]=new Foititis("Brett","Paterson", (short) 3,2022);
         MyStudentArray[2]=new Foititis("Sumayya","Boyd", (short) 4,2023);
         MyStudentArray[3]=new Foititis("Serenity","Wallis", (short) 6,2022);
